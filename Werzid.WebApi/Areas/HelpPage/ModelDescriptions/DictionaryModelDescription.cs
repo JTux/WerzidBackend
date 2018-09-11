8444c90a6bb2eed33629bf2206793bd1e87868ea
Joshua Tucker <thejltucker@gmail.com>
2018-09-11 14:01:06 -0400

    Add project files.

---
+++ Werzid.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Werzid.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Werzid.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Werzid.WebApi.WebApiApplication" Language="C#" %>
