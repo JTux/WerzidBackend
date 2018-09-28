6e81336b8d2652a2cd4e627ef2cde0181ca2994e
Joshua Tucker <thejltucker@gmail.com>
2018-09-27 23:10:14 -0400

    Changed password requirements

--- Werzid.WebApi/App_Start/IdentityConfig.cs
+++ Werzid.WebApi/App_Start/IdentityConfig.cs
@@ -29,11 +29,11 @@ namespace Werzid.WebApi
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
+                RequiredLength = 4,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
